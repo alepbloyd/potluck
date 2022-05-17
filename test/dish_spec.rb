1a3178187a31fd515aa29f903ee14ebd382c28ab
alepbloyd <alexanderleeboyd@gmail.com>
2022-05-17 10:50:51 -0400

    Test: .name returns name

--- test/dish_spec.rb
+++ test/dish_spec.rb
@@ -7,7 +7,12 @@ RSpec.describe Dish do
     new_dish = Dish.new("Couscous Salad", :appetizer)
 
     expect(new_dish).to be_instance_of(Dish)
+  end
 
+  it 'returns dish name with .name' do
+    new_dish = Dish.new("Couscous Salad", :appetizer)
+
+    expect(new_dish.name).to eq("Couscous Salad")
   end
 
 end
